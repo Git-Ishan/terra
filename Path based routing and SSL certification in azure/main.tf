4c9ee82923da47adfb8f46106d18e70e99cf17b1
Ishan <142745065+Git-Ishan@users.noreply.github.com>
2025-09-28 23:05:52 +0530

    Create main.tf

---
+++ Path based routing and SSL certification in azure/main.tf
@@ -0,0 +1,145 @@
+
+variable "pfx_data_base64" {
+  description = "The Base64 encoded data of the PFX certificate file."
+  default = "MIIJ/wIBAzCCCbUGCSqGSIb3DQEHAaCCCaYEggmiMIIJnjCCBBIGCSqGSIb3DQEHBqCCBAMwggP/AgEAMIID+AYJKoZIhvcNAQcBMFcGCSqGSIb3DQEFDTBKMCkGCSqGSIb3DQEFDDAcBAg9kJIwYBzmEgICCAAwDAYIKoZIhvcNAgkFADAdBglghkgBZQMEASoEECV9IWf/VQb8ZfH7yHkkeG2AggOQ6AcoozMzVuavTi+44Vx7CyxBv3g0T9630SiOLM4fCYO2+P/YpN0nq0/rd8MGFaLA3GuyXmDt/IcVi6NbAKqzxsPJ0AK4lRSe8c4ObwwVfhPUxVNQrkbC2NcL8t45LU/jahH14m1e2j+T8cL3gj5m+mfCNiVsXDaH3MkVNbb0v2tnTFtuom/mJGc4KIFWdSanIQZv8xvNlUiKlLB7uJtLwWW+6MIaTmDJ46+aqQRPWsxTWdqhW5XmM6ejXKPtmIYt98viv5C7as6vatqZmg9VFYjwo28LEwj2Mrx5WmAfoDxpO4CTXSQV1S1dFCtPRK6uOd+2nUBUZ1dqPYHQiIgJT40EszBAwk4aVUkIPe/krYPE6jSvaEJN8QrN3vRwDLfOyeJ0pz6u5LKno6LR+kxzvd4FEMPFTehtkYOwlJulMsLyPipm3FLMbGWqOrkYT7Tvxmt3AGSBpFXsclfDedvraBwazA5KMMcr7X9qo3s0dBbBVCh5NQwXiAf3l8pBnnGPoTyrmbIGy6vkIKAPgSYatze+y0w+icwp4BBc+iT1KQrj05O4JhpnjT3TCsZWkA/M8v4piIcezT6POloVEE+L1mt0bROBe7NKDnr6HjVKCQ/jkUFyiffTtiuf2l7S0jTE3Qk76qjxDECPwb1P0SXUqy+DIRB9WLpEwajXySu3fNzVjzD8+1uufF9wIgTxemTEGa4xhlhGqEjzi3i1AhF41S3+dZBhvjOu7xXiSjaLAckSekDDALgFSGL9vHWIrFkYv6ZmwMa9v9z9MJK0mxMT74Dkk3FQ0uALoCh/ZHwhBRZxZOT11fyPJQHDhcQwkE0H2t1DBg1qg5Oa6bMirF9Q768ENUKtWIvtk2shgPOJQELSQ+JfPlNVUKfaYJiwzFuB+0QXXANzk3YbOcmcZjmPH4JfLjXtpFgKzeF8/6EBFQ3dh/WGqEuCfbKUSjuunUjhL+YINe7z2VfE7bI+Mabw38G24/g1/lSX8cdhktEWXf21tv/XjoU+/ogSvp1YoxrEMUr7CsQABNs7TiTNpBLGpIgzEAB0EDjMJWZoei1/tu5xNuQ0Y38s6cbrMSkEzzYlI9eNp7uwU0uT3odjodQX4QC8z5OY9H8zrofi3I+8zvQCDnb7jyjCaW6CDP0NVKKmljA8SR0gs2Z/gyTgqG0Hw9rROI7wtt+TF+wxKT5PtK1v8CGELJNhDQwd4Hem1v57MIIFhAYJKoZIhvcNAQcBoIIFdQSCBXEwggVtMIIFaQYLKoZIhvcNAQwKAQKgggUxMIIFLTBXBgkqhkiG9w0BBQ0wSjApBgkqhkiG9w0BBQwwHAQIQoglJgyplC4CAggAMAwGCCqGSIb3DQIJBQAwHQYJYIZIAWUDBAEqBBAAePn3ijtIkf0PRVpKAu6eBIIE0GIQzXTz7MsN94i26JLaawdRB46M4xn7xp8jeTd1mYxx/BKgw6CBJ5GRGGLxoC/h3KtJZwIuDHFri1NSFnoLoTfwgSrlirPO2ikA+HL9ReDYRpuOJ0SOevRmL2si2cRr1ySZGJM/j1KqZms2qyoAYca5PXNVEiBC2SZAKZZdH7i+wzVnv4AFXwyQ/S9LK0te310CLgLQ7vWmUd3sLSJCQ/R61U+3VPv9t6/udPv3kd5RJpTIrn+9LGKumGXsNtojtMleHJ8ZrXxLIsuu4Xca31tdZ/wrDpwaL0qD9ZRE7PgLho1S5t2EV/V5/QTIKf6uwIaKHJxc1sz7OY/U+y4brmSpU1pwLAb2uB0HCEBiZwiA3yX4A3Zk+2rcUx3Cpy9nWQSfdI2FmEFQNlbRrPmZMGTVnA2UlCDb6FgMteWpLRZfZ2GlQr240NglXfHCuRiiPIYchrQzmVmGjqrsy9lnxmaNKsleLW2cmTeJ0pfe+tZNCw+/1XasUZadGyKYHEBiCDKcg4Fnu4s9bYo3ZgMfdEcQp10G5hmI11WLjY5xVN0PzJC/swv0YeV67hR1lVKANZeEdjYLkr7ULjL7oqI58gHxC4L7dUYl925rPyPNF6F75Rol2fM6fUZZoxVWvoNoYUGyc0xmY1zOIIL5tXu3uDo6deYUM/kBM6A3F6ugiIq4c59PDhMVN11Finpsd3zi3Hr0v/WrzVvQkGAkIzBf0AGHMfZoO1HbrZxk/iWRcwSQ1W2FREku0KIT+DayPb4FJsQLU/DQ8Me6WUUQfW/NYwG0uI0lC7UMTU/2T6bkJ9M5x4BQ0Esyr2myGAiTMXfsfDuVHMG1a45lxX7obMhONcpVEv2GUvpllipEMcUxnsTDwvjrQRtWfjLr19hCx6cNfNfOXT/mzlYNmQt3V/oW1Xz4K3wFVsK9k94gDtIL3K28M1NPbzaGxj/h9WM6RRdHKvBFiCvOboORcS6KZBijT6pGI5pM+nJSUUyWVWBfU+maGahGXQXiVTwrwFX+tTgjUNUcqDD9BQN35Dxk0rg4R7pl0ZVLdBH5q1NOVEXHcIYkxIeNm5MAyVmd9LHj4EEA30ODXk4nIA0XkzgCGiK0ps8gGPEPX65BZ+jnUZoLrKUK2ZI7HfIsgVrGr1JQn0HdSKBYUwPN968ONRNCW5bhZzkaN2t8Twwcwu1ZTbtuD5EideLVUzvclwPJV4S/p4h6gho+GRJnq+mbyj4rWiI8gh4+QLGeX8GFK3chkJ61/aFScn8fvCMqfIfzs6EDIssp80Zi2R2g5YOzMF7ltPFjfw3e8NVd7uUDftPsMIAPG5RFWlH9drGfuIPSipsIUWqRx06WAlLcq0ebDLCc4EMRfbKWZTURboOrtUx/vYoIqvxtY2XIo7bePynFPy1wz/EzOt+inG88BuMQF7IMxGjElQxaWIpOGNGuC/o+8Z+b53CuSlZaFc5HWPeLFbgOsNT/49/YZGvM8x6U8t2aOxRLZNpWIZ4kwJYt/6+SxXgOF77po8wUNT7B7PM6ifXcgYxqpWZUBTBVp08ug2HhFRVJY31u3+6p/wMN3OYAPamg1LczEJUFQaDLrMq5l3dBqOZgc+NXdPdi8fxV5L/SLhMSuL4AHfOEh7OGU7mB3yEEX8BZMSUwIwYJKoZIhvcNAQkVMRYEFNb8L5yFYVbMYI9dk28HCMuVsuNRMEEwMTANBglghkgBZQMEAgEFAAQgov4WMmPlbmappZYwvS/0rtjNY1fsLXc8IPRB6C7lgFYECBljilJErHAOAgIIAA=="
+  sensitive   = true
+}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.46.0"
+    }
+  }
+}
+provider "azurerm" {
+  subscription_id = "5d228b2b-b879-4a16-a9a9-d0ceb7f20fe4"
+  features {}
+}
+# Variables for your SSL Certificate
+
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "appgw-vnet"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "appgw_subnet" {
+  name                 = "AppGatewaySubnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+resource "azurerm_public_ip" "appgw_pip" {
+  name                = "appgw-public-ip"
+  sku                 = "Standard"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+}
+resource "azurerm_dns_zone" "example" {
+  name                = "my-app-gateway-project.com"
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_application_gateway" "example" {
+  name                = "my-app-gateway"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+
+  sku {
+    name     = "Standard_v2"
+    tier     = "Standard_v2"
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name      = "appGatewayIpConfig"
+    subnet_id = azurerm_subnet.appgw_subnet.id
+  }
+
+  frontend_port {
+    name = "https-port"
+    port = 443
+  }
+
+  frontend_ip_configuration {
+    name                 = "appGatewayFrontendIp"
+    public_ip_address_id = azurerm_public_ip.appgw_pip.id
+  }
+  ssl_certificate {
+    name     = "my-ssl-cert"
+    data     = filebase64("${path.module}/mycert.pfx")
+    password = "123"
+  }
+  backend_address_pool {
+    name = "image-pool"
+  }
+
+  backend_address_pool {
+    name = "video-pool"
+  }
+  backend_http_settings {
+    name                  = "image-http-settings"
+    cookie_based_affinity = "Disabled"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 20
+  }
+
+  backend_http_settings {
+    name                  = "video-http-settings"
+    cookie_based_affinity = "Disabled"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 20
+  }
+  http_listener {
+    name                           = "https-listener"
+    frontend_ip_configuration_name = "appGatewayFrontendIp"
+    frontend_port_name             = "https-port"
+    protocol                       = "Https"
+    ssl_certificate_name           = "my-ssl-cert"
+  }
+  url_path_map {
+    name                               = "path-based-rules"
+    default_backend_address_pool_name  = "image-pool"
+    default_backend_http_settings_name = "image-http-settings"
+
+    path_rule {
+      name                       = "images-rule"
+      paths                      = ["/images/*"]
+      backend_address_pool_name  = "image-pool"
+      backend_http_settings_name = "image-http-settings"
+    }
+
+    path_rule {
+      name                       = "videos-rule"
+      paths                      = ["/videos/*"]
+      backend_address_pool_name  = "video-pool"
+      backend_http_settings_name = "video-http-settings"
+    }
+  }
+  request_routing_rule {
+    name               = "main-routing-rule"
+    rule_type          = "PathBasedRouting"
+    http_listener_name = "https-listener"
+    url_path_map_name  = "path-based-rules"
+    priority           = 100
+  }
+}
+
+# --- 3. Final DNS Record ---
+resource "azurerm_dns_a_record" "example" {
+  name                = "www"
+  zone_name           = azurerm_dns_zone.example.name
+  resource_group_name = azurerm_resource_group.example.name
+  ttl                 = 300
+  target_resource_id  = azurerm_public_ip.appgw_pip.id
+}
